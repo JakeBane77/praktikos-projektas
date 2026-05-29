6005d5bbf74dc9d3e1a4e9fd8e73582585b2ae85
JakeBane <arnasx.lt@gmail.com>
2026-05-29 14:12:14 +0300

    Added function that gets user system time which will be used in immersive mode for sun/moon location.

---
+++ resources/js/lib/userSystemTime.ts
@@ -0,0 +1,28 @@
+export type UserSystemTime = {
+    date: Date;
+    iso: string;
+    timestamp: number;
+    timezone: string;
+    timezoneOffsetMinutes: number;
+    hour: number;
+    minute: number;
+    second: number;
+    hourDecimal: number;
+};
+
+export function getUserSystemTime(date = new Date()): UserSystemTime {
+    return {
+        date,
+        iso: date.toISOString(),
+        timestamp: date.getTime(),
+        timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        timezoneOffsetMinutes: date.getTimezoneOffset(),
+        hour: date.getHours(),
+        minute: date.getMinutes(),
+        second: date.getSeconds(),
+        hourDecimal:
+            date.getHours() +
+            date.getMinutes() / 60 +
+            date.getSeconds() / 3600,
+    };
+}
