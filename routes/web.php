64f94eaa56c05f3eb0b85eb582bb7310437dd1c4
JakeBane <arnasx.lt@gmail.com>
2026-06-15 13:35:20 +0300

    Fixed pint code style issues

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\AllianceController;
 use App\Http\Controllers\AllianceChatController;
+use App\Http\Controllers\AllianceController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
